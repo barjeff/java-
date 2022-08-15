c2ad31716ce41e215bee600e070ef8dc78fc573b
jefferson magallanes <jeffersonmaga97@gmail.com>
2022-08-15 16:23:07 -0300

    aprendiendo

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,54 @@
 public class Main {
     public static void main(String[] args) {
-    Coche newcocheBlue = new Coche();
-    newcocheBlue.addpuertas();
-    System.out.println(newcocheBlue.puertas);
     
-    var valor =  suma(4,5,6);
-        System.out.println(valor);
-    }
-    public static int suma ( int a, int b,int c) {
-        return a + b + c;
+    int numeroIf  = 5;
 
+    if (numeroIf > 0) {
+        System.out.println(numeroIf);
+    } else if (numeroIf < 0) {
+        System.out.println(numeroIf);
+    } else if (numeroIf == 0) {
+        System.out.println(numeroIf);
+    }
+  
+    int  numeroWhile = 3;
+    while ( numeroWhile < 3) {
+        numeroWhile =  numeroWhile + 1;
+        System.out.println( numeroWhile);
+    }
+
+
+    int  numberoDoWhile = 3;
+    do {
+        numberoDoWhile = numberoDoWhile + 1;
+    }  while (numberoDoWhile < 4);
+
+    for (int numeroFor = 0; numeroFor <=3; numeroFor = numeroFor + 1) {
+        System.out.println(numeroFor);
+    }
+
+    int values[] = new int [5];
+
+    for (int i = 0; i < values.length; i++ ) {
+        System.out.println(values);
+    }
+    var estacion = "otoño";
+    switch (estacion) {
+        case "invierno":
+            
+            break;
+        case "verano":
+            
+            break;
+        case "otoño":
+        System.out.println("es: otoño" );
+            break;
+        default:
+        System.out.println("La contamincacion ambientas acabo con las estaciones" );
+            break;
+    }
     }
 }
 
 
 
-class Coche {
-    public int  puertas = 5;
-
-    public  void addpuertas() {
-        this.puertas ++;
-    }
-} 
