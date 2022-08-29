b7300cec5ee2b899972a6818b667758d6a4e08f1
jefferson magallanes <jeffersonmaga97@gmail.com>
2022-08-29 17:30:25 -0300

    tema 8

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,46 @@
+
 public class Main {
-    public static void main(String[] args) {
     
-    int numeroIf  = 5;
+    public static void main (String[] args){
+        Persona persona = new Persona ();
+        persona.setNombre("jeff");
+        persona.setEdad(25);
+        persona.setTelefono(43566778);
+       
+        System.out.println(persona.getNombre()); 
+        System.out.println(persona.getEdad()); 
+        System.out.println(persona.getTelefono());
 
-    if (numeroIf > 0) {
-        System.out.println(numeroIf);
-    } else if (numeroIf < 0) {
-        System.out.println(numeroIf);
-    } else if (numeroIf == 0) {
-        System.out.println(numeroIf);
-    }
-  
-    int  numeroWhile = 3;
-    while ( numeroWhile < 3) {
-        numeroWhile =  numeroWhile + 1;
-        System.out.println( numeroWhile);
     }
 
 
-    int  numberoDoWhile = 3;
-    do {
-        numberoDoWhile = numberoDoWhile + 1;
-    }  while (numberoDoWhile < 4);
-
-    for (int numeroFor = 0; numeroFor <=3; numeroFor = numeroFor + 1) {
-        System.out.println(numeroFor);
-    }
-
-    int values[] = new int [5];
-
-    for (int i = 0; i < values.length; i++ ) {
-        System.out.println(values);
-    }
-    var estacion = "otoño";
-    switch (estacion) {
-        case "invierno":
-            
-            break;
-        case "verano":
-            
-            break;
-        case "otoño":
-        System.out.println("es: otoño" );
-            break;
-        default:
-        System.out.println("La contamincacion ambientas acabo con las estaciones" );
-            break;
-    }
-    }
 }
 
+class Persona {
+    private String nombre;
+    private int edad;
+    private int telefono;
 
+    public void setNombre (String nombre){
+        this.nombre = nombre;
+    }
 
+    public String getNombre() {
+        return this.nombre; 
+    }
+    public void setEdad (int edad) {
+        this.edad = edad; 
+    }
+
+    public int getEdad(){
+        return this.edad;
+    }
+
+    public void setTelefono (int telefono){
+        this.telefono = telefono;
+    }
+
+    public int getTelefono (){
+        return this.telefono;
+    }
+}
